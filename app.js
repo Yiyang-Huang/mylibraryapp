b185b581b7b23c9fd3384c83bbd1a4ce9d858d88
Yiyang <yiyahuang@student.edu.au>
2021-04-13 21:38:19 +1000

    dynamic port number for app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express')
 // const bodyParser = require('body-parser')
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+// app.use(express.json());
+// app.use(express.urlencoded({
+//   extended: true
+// }));
 
 const authorRouter = require('./routes/authorRouter')
 
@@ -17,6 +17,6 @@ app.post('/author-management', authorRouter)
 
 app.use('/author-management', authorRouter)
 
-app.listen(3000,() => {
-    console.log('The library app is listening on port 3000!')
+app.listen(process.env.PORT || 8080,() => {
+    console.log('The library app is listening on port 8080!')
 })
